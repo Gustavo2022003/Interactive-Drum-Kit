fd240148b17f31a467428fb35dac1a551f4b45e2
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-03-09 22:58:56 -0300

    Update index.js

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+
+for (let i = 0; i < document.querySelectorAll("button.drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    });
+}
