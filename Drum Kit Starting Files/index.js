1bd24c45f5239efb93283edfebc7fd4847892e7c
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-03-10 20:30:40 -0300

    update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,10 @@
 
 for (let i = 0; i < document.querySelectorAll("button.drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        alert("I got clicked");
+        this.style.color = "white";
+
     });
 }
 
+// var tomOne = new Audio("sounds/tom-1.mp3");
+// tomOne.play();
