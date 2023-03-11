f4516e17f634098a24f85c9453529112e71b790f
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-03-10 21:48:00 -0300

    slk bglh tá ficando potente
    Fazendo os botões funcionarem com som

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,51 @@
 
 for (let i = 0; i < document.querySelectorAll("button.drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        this.style.color = "white";
+        let buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML){
+            case "w":
+                let tomOne = new Audio("sounds/tom-1.mp3");
+                tomOne.play();
+                break;
+
+            case "a":
+                let tomTwo = new Audio("sounds/tom-2.mp3");
+                tomTwo.play();
+                break;
+
+            case"s":
+                let tomThree = new Audio("sounds/tom-3.mp3");
+                tomThree.play();
+                break;
+
+            case"d":
+                let tomFour = new Audio("sounds/tom-4.mp3");
+                tomFour.play();
+                break;
+
+            case "j":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "k":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "l":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+            default:
+                console.log(buttonInnerHTML)
+        }
 
     });
 }
 
-// var tomOne = new Audio("sounds/tom-1.mp3");
-// tomOne.play();
+document.addEventListener("keydown", function(event) {
+    console.log(typeof(event));
+});
